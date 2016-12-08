b4895d46cb3003a4cb4685a4f751abf57ed0c2d9
cxj <cxj@cxjdeMacBook-Pro.local>
2016-12-08 18:46:37 +0800

    1.1.1

--- controllers/action.js
+++ controllers/action.js
@@ -218,7 +218,7 @@ exports.updateSubPageContent = function(req, res) {
 					}
 					res.render('subpage_ueditor', {
 						user: req.session.user? req.session.user : {username: ''},
-						subPage: subpage,
+						subPage: the_sub,
 						msg: '修改成功'
 					})
 				return
